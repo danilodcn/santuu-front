615fe7eea8ca7bdfa7ddb16b485841034cee8599
Vinicius Cainã <vinicius.barros@wtechnology.com.br>
2022-05-11 13:18:34 +0000

    Criando ProposalValues e components

---
+++ src/components/BoxContent.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card class="sad" elevation="10">
+    sadasdsadas
+    <slot></slot>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class BoxContent extends Vue {}
+</script>
+
+<style scoped>
+.sad {
+  font-family: Poppins;
+  background-color: red;
+}
+</style>
--- src/views/simulation/ProposalValues.vue
+++ src/views/simulation/ProposalValues.vue
@@ -1,10 +1,18 @@
 <template>
-  <div></div>
+  <div class="proposal-values">
+    <BoxContent>asdasd</BoxContent>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import BoxContent from "@/components/BoxContent.vue"; // @ is an alias to /src
 
+@Component({
+  components: {
+    BoxContent,
+  },
+})
 @Component
 export default class HomeView extends Vue {}
 </script>
