a969e57a09efdeb6d34733d52fb1b3c2facf4090
Norteve <everton_menezes_@outlook.com>
2022-06-16 21:14:52 +0000

    Fazer requisição do eventos

--- src/types/events.ts
+++ src/types/events.ts
@@ -1,7 +1,11 @@
 export interface IEvents {
-    name: string;
-    description: string;
-    poster: string;
-    initial_date: string;
-    final_date : string;
-  }
+  name: string;
+  description: string;
+  poster: string;
+  initial_date: string;
+  final_date: string;
+}
+
+export interface IFormItems {
+  events: IEvents[];
+}
