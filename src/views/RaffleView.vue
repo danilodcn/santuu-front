3b0d4b4410e385fb25b403b47af0fa9abf1a9e30
Danilo Nascimento <daconnas.dcn@gmail.com>
2022-06-25 16:09:46 +0000

    configurando rotas e adicionanco view de sorteio

---
+++ src/views/RaffleView.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container fluid>
+    <v-card>
+      <v-row align="center" justify="space-around">
+        <v-img src="@/assets/presente.png" v-bind="imgProps" />
+        <span class="text-h3 mx-5">Sorteador</span>
+        <v-img src="@/assets/presente.png" v-bind="imgProps" />
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { BaseComponent } from "@/utils/component";
+
+export default class RaffleView extends BaseComponent {
+  get imgProps() {
+    const height = 100;
+    return {
+      "max-width": height,
+      "max-height": height,
+      "aspect-ratio": 1,
+      height: height,
+    };
+  }
+}
+</script>
+
+<style></style>
