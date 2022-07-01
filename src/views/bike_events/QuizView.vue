686b88f8dd299d84b606b116cefa513b558195e4
Danilo Nascimento <daconnas.dcn@gmail.com>
2022-07-01 00:09:16 +0000

    injetando html vindo do backend na view

--- src/views/bike_events/QuizView.vue
+++ src/views/bike_events/QuizView.vue
@@ -3,10 +3,9 @@
     <v-card class="pa-5 pa-md-15">
       <v-card-title v-text="quiz.title" class="text-h4" />
       <v-spacer />
-      <v-card-subtitle
-        v-text="quiz.description"
-        class="text-subtitle-1 text-justify"
-      />
+      <v-card-subtitle class="text-subtitle-1 text-justify">
+        <div v-html="quiz.description" />
+      </v-card-subtitle>
       <quiz-form ref="form" :quiz="quiz" v-model="model"></quiz-form>
       <v-card-actions class="justify-end">
         <v-btn @click="clearAll()" class="px-7">Limpar</v-btn>
