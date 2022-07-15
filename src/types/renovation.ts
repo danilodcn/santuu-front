1085daaf886f110cc72aba38009c0ea0f8973d7e
Norteve <everton_menezes_@outlook.com>
2022-07-15 02:24:26 +0000

    types da renovação

---
+++ src/types/renovation.ts
@@ -0,0 +1,51 @@
+import { IProgram } from "@/types/program";
+export interface IPlan {
+  name: string;
+  amount: number;
+}
+export interface ICoverage {
+  id: number;
+  name: string;
+  deductible_text: string;
+  legal_text: string;
+  amount: number;
+  lmi: number;
+  enabled: boolean;
+  is_fixed: boolean;
+  order: number;
+}
+
+export interface IAssociateData {
+  id: number;
+  brand: string;
+  category: string;
+  model: string;
+  price: number;
+  plan: IPlan;
+}
+
+export interface IProposalBid {
+  id: number;
+  number_of_installments: number;
+  amount: number;
+  discount_amount: number;
+  gross_amount: number;
+  minimal_amount: number;
+}
+
+export interface IProposalRenovation {
+  id: number;
+  created_at: string;
+  proposal_coverages: ICoverage[];
+  associate_data: IAssociateData[];
+  iof: number;
+  gross_insurance_premium: number;
+  program: IProgram;
+  proposal_bids: IProposalBid[];
+  insurance_premium_discount: number;
+  insurance_premium: number;
+  calculated_insurance_premium: number;
+  deductible_enabled: boolean;
+  number_of_installments: number;
+  voucher: string;
+}
