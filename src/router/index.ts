608e512defbc18d8e8fc9d4fd92c283d5008c1bd
Norteve <everton_menezes_@outlook.com>
2022-07-19 23:19:55 +0000

    Ordenando e adicionando proposal-values

--- src/router/index.ts
+++ src/router/index.ts
@@ -49,14 +49,21 @@ const routes: Array<RouteConfig> = [
 
   // RENOVAÇÃO
   {
-    path: "/renovation/summary-certificate/:proposal_id/", //Resumo do Certificado
-    name: "renewal-summary",
-    component: () => import("@/views/proposal_renovation/RenovationView.vue"),
-  },
-  {
-    path: "/renovation/certificates",
+    path: "/renovation/certificates", //Meus Certificado
     name: "certificates",
     component: () => import("@/views/proposal_renovation/CertificateView.vue"),
+  },
+  {
+    path: "/renovation/summary-certificate/:proposal_id/", //Resumo do Certificado
+    name: "summary-certificate",
+    component: () =>
+      import("@/views/proposal_renovation/SummaryCertificate.vue"),
+  },
+  {
+    path: "/renovation/proposal-values/:proposal_id/",
+    name: "proposal-values",
+    component: () =>
+      import("@/views/proposal_renovation/ProposalValuesView.vue"),
   },
   {
     path: "/renovation/update-data/:proposal_id/",
