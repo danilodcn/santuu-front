845ab53f6ae43ea5a681f0de269d333c74beaf42
Norteve <everton_menezes_@outlook.com>
2022-07-19 23:19:15 +0000

    Adicionando links do botões

---
+++ src/views/proposal_renovation/ProposalValuesView.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container fluid>
+    <proposal-values
+      :proposal_id="proposal_id"
+      title="Valores da Proposta"
+      back_lick="/renovation/certificates"
+      :next_link="`/renovation/update-data/${proposal_id}`"
+    >
+    </proposal-values>
+  </v-container>
+</template>
+
+<script lang="ts">
+import ProposalValues from "@/components/ProposalValues.vue";
+import { Vue, Component } from "vue-property-decorator";
+import { BaseComponent } from "@/utils/component";
+
+@Component({ components: { ProposalValues } })
+export default class Available extends BaseComponent {
+  proposal_id = this.$route.params.proposal_id;
+}
+</script>
