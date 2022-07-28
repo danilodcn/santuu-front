eb4c8624c2a6d0eb3a90cd403c7e04556d761290
Norteve <everton_menezes_@outlook.com>
2022-07-28 15:26:02 +0000

    Finalizando Tela de pagamento

--- src/views/proposal/PaymentView.vue
+++ src/views/proposal/PaymentView.vue
@@ -1,18 +1,20 @@
 <template>
-  <v-container fluid>
-    <proposal-values
-      :proposal_id="proposal_id"
-      title="Resumo da proposta"
-      page_title="Pagamento"
-    >
-    </proposal-values>
-
+  <v-container class="box-content" fluid>
     <v-container>
       <v-card>
+        <DetailBox
+          :table="tableDescription"
+          :key="keyResume"
+          class="pb-0 table-Description"
+        >
+          Finalizar Renovação <br />
+          <br />
+          <h6>Segue um resumo da sua renovação:</h6>
+        </DetailBox>
         <v-card-title class="main-color">
-          <span class="mt-5 mx-4"> Termos </span>
+          <span class="mt-5 mx-4 pb-0"> Termos </span>
         </v-card-title>
-        <v-col class="mx-4 px-4">
+        <v-col class="mx-4 px-4 pb-0">
           <v-checkbox
             v-for="(item, i) in termsAndConditions"
             :key="`selected-${i}`"
@@ -21,16 +23,13 @@
             v-model="item.accept"
           />
         </v-col>
+        <payment-form
+          v-model="paymentModel"
+          :proposal="proposal"
+          :terms="termsAndConditions"
+          :linkNext="`/web/associate/proposal/payment/sucess?proposal=${proposal_id}&email_check=true&cell_check=true`"
+        ></payment-form>
       </v-card>
-    </v-container>
-
-    <v-container>
-      <payment-form
-        v-model="paymentModel"
-        :proposal="proposal"
-        :terms="termsAndConditions"
-        :linkNext="`/web/associate/proposal/payment/sucess?proposal=${proposal_id}&email_check=true&cell_check=true`"
-      ></payment-form>
     </v-container>
   </v-container>
 </template>
@@ -41,9 +40,25 @@ import PaymentForm from "@/components/PaymentFom.vue";
 import { Component } from "vue-property-decorator";
 import { BaseComponent } from "@/utils/component";
 import { ProposalService } from "@/api/proposal";
+import DetailBox, {
+  IDetailedInfo,
+  ITableRow,
+} from "@/components/shared/DetailBox.vue";
+import { formatDateDetail, formatPrice } from "@/utils/utils";
 
 type Proposal = {
+  iof: number;
+  gross_insurance_premium: number;
+  insurance_premium_discount: number;
   insurance_premium: number;
+  proposal_duration: string;
+};
+
+type Discount = {
+  discount_percent_renew: string;
+  calculated_insurance_premium: number;
+  discount: number;
+  new_iof: number;
 };
 
 type Terms = {
@@ -51,25 +66,53 @@ type Terms = {
   accept?: boolean;
 };
 
+const titlesDescription: IDetailedInfo[] = [
+  {
+    value: "Data e hora do Início",
+    description: "",
+  },
+  {
+    value: "IOF",
+    description: "",
+  },
+  {
+    value: "Valor",
+    description: "",
+  },
+  {
+    value: "Desconto de Renovação",
+    description: "",
+  },
+  {
+    value: "Valor Total",
+    description: "",
+  },
+];
+var itemsDescription: ITableRow[] = [
+  {
+    values: [],
+  },
+];
+
+const tableDescription = {
+  titles: titlesDescription,
+  rows: itemsDescription,
+};
 const proposalService = new ProposalService();
 
-@Component({ components: { ProposalValues, PaymentForm } })
+@Component({ components: { ProposalValues, PaymentForm, DetailBox } })
 export default class Available extends BaseComponent {
   proposal_id = this.$route.params.proposal_id;
   paymentModel = {};
-  proposal? = {} as Proposal;
+  proposal = {} as Proposal;
+  keyResume = 0;
+  tableDescription = tableDescription;
+  discount = {} as Discount;
 
   termsAndConditions: Terms[] = [
     {
       message:
         "Ao marcar este campo, concordo com os dados e valores acima para efetuar o pagamento da proposta.",
-    },
-    {
-      message:
-        "Aceito receber comunicação via e-mail com ofertas e benefícios do Clube Santuu e Clube Decatlhon.",
-    },
-    {
-      message: "Aceito receber comunicação via celular.",
     },
   ];
 
@@ -77,16 +120,66 @@ export default class Available extends BaseComponent {
 
   async getProposal() {
     this.proposal = await proposalService.getSimpleProposal(this.proposal_id);
+    this.discount = await proposalService.getDiscountRenew(this.proposal_id);
+    this.setValues();
   }
   async created() {
     this.changeLoading(true);
     await this.getProposal();
     this.changeLoading(false);
   }
-
+  setValues() {
+    const description = [
+      {
+        value: formatDateDetail(this.proposal.proposal_duration),
+        description:
+          "A renovação terá vigência a partir do termino da cobertura atual e não da data de pagamento.",
+      },
+      {
+        value: formatPrice(this.discount.new_iof),
+        description: "",
+      },
+      {
+        value: this.proposal.insurance_premium,
+        description: "",
+      },
+      {
+        value: `${formatPrice(this.discount.discount)}(${
+          this.discount.discount_percent_renew
+        }%)`,
+        description: "",
+      },
+      {
+        value: formatPrice(this.discount.calculated_insurance_premium),
+        description: "",
+      },
+    ];
+    this.tableDescription.rows[0].values = description;
+  }
   mounted() {
     console.log(this.termsAndConditions);
     this.acceptTerms = this.termsAndConditions.map((item) => !!item.accept);
   }
 }
 </script>
+<style lang="scss" scoped>
+.box-content {
+  margin: auto;
+  margin-top: 7px;
+  max-width: 1080px;
+  padding: 20px;
+}
+.table-Description {
+  text-align: center !important;
+  max-width: 1000px;
+}
+
+.main-color {
+  color: #c1c523;
+}
+</style>
+<style lang="css">
+.table-Description:last-child {
+  background-color: #c1c523 !important;
+}
+</style>
