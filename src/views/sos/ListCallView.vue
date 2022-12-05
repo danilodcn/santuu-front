e65a79e2957e30e3e44fab9ce6ede271468d2d39
Everton <everton_menezes_@outlook.com>
2022-12-05 03:15:04 +0000

    Padronizando o nome do arquivo

--- src/views/sos/ListCallView.vue
+++ src/views/sos/ListCallView.vue
@@ -10,7 +10,7 @@
             <SosCard :data="orders" />
           </v-col>
         </template>
-        <h4 class="no-list" v-if="orders">Nenhum Chamando</h4>
+        <h4 class="no-list" v-if="orders.leght > 0">Nenhum Chamando</h4>
       </v-row>
     </div>
     <v-divider class="mt-10"></v-divider>
@@ -30,16 +30,33 @@ import { IOrder } from "@/types/sos";
   components: { SosCard },
 })
 export default class Available extends Vue {
-  orders = {} as IOrder;
+  orders = {
+    id: 1,
+    associate: 22,
+    service_bike: 20,
+    service_bike_model: "service_bike_model",
+    service_bike_brand: "service_bike_brand",
+    service_type: 5,
+    service_text: "string",
+    service_bike_lane: 2,
+    service_ref_location: "Marginal Pinheiros, São Paulo-SP",
+    mechanic: 5,
+    service_status: "Aberto",
+    service_protocol: "service_protocol",
+    status_text: "status_text",
+    coordinates: "-23.580483258417278, -46.661190902681504",
+    mechanic_name: "João",
+    rating: 3.5,
+  };
 
-  async getListCall() {
-    const response = await sosService.getListCall();
-    if (response.error) {
-      console.log(console.error());
-    } else {
-      this.orders = response;
-    }
-  }
+  // async getListCall() {
+  //   const response = await sosService.getListCall();
+  //   if (response.error) {
+  //     console.log(console.error());
+  //   } else {
+  //     this.orders = response;
+  //   }
+  // }
 }
 </script>
 
